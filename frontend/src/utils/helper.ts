689e116d49f3d23a2b940c546caf4e25300a77a9
Prittha Dutta <pritthadatta@gmail.com>
2024-08-19 13:20:28 +0530

    Added Error alert

--- frontend/src/utils/helper.ts
+++ frontend/src/utils/helper.ts
@@ -1,14 +1,18 @@
 export const fetchData = async(url: string, method: string, data?: Object)=>{
+    try {
+        const res = await fetch(`${process.env.REACT_APP_BACKEND_URI}${url}`,{
+            method: method,
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: method === "GET"? undefined : JSON.stringify(data)
+        })
     
-    const res = await fetch(`${process.env.REACT_APP_BACKEND_URI}${url}`,{
-        method: method,
-        headers:{
-            "Content-Type": "application/json"
-        },
-        body: method === "GET"? undefined : JSON.stringify(data)
-    })
-
-    const output = await res.json();
-
-    return output;
+        const output = await res.json();
+    
+        return output;
+    } catch (error) {
+        console.error(error);
+        alert(error)
+    }
 }
